d1cc78e95c91b98f7623cc18460df1d8c157ecb2
sm <nebulas153@gmail.com>
2026-06-21 16:32:37 +0000

    update

--- Run_for_life/run.js
+++ Run_for_life/run.js
@@ -9,8 +9,9 @@ let x = 0;
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
    //ctx.fillRect(100, 50, 100, 100); 
-    ctx.drawImage(playerImage, 0 , 0);
-    x++;
+   //ctx.drawImage(image , sx ,sy , sw , sh ,dx,dy,dw ,dh);
+    ctx.drawImage(playerImage, 50 , 50 , CANVAS_WIDTH , CANVAS_HEIGHT , 100 , 100 , 100 , 100);
+    
     requestAnimationFrame(animate);
 };
 animate();
